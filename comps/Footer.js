d1f6daf7bc3b99c99bf96657ed069e006cb507b4
Matthieudegny <matthieu.dev.degny@gmail.com>
2023-01-19 22:19:29 +0100

    creation component

---
+++ comps/Footer.js
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <div>Cpyright 2021 Ninja List</div>;
+};
+
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,15 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
+import Navbar from "@/comps/Navbar";
+import Footer from "@/comps/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>HomePage</h1>
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae
@@ -21,6 +24,7 @@ export default function Home() {
         earum at saepe, dolorum ipsum mollitia! Praesentium voluptatem totam vel
         tenetur!
       </p>
+      <Footer />
     </div>
   );
 }
